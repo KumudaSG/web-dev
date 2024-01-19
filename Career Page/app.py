3d84ec6c50bbdcb28dcf8dbd2ba6e1d55b41e4c1
Kumuda S <ksg6@illinois.edu>
2024-01-18 19:37:48 -0600

    Better structure, added templating

---
+++ Career Page/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+JOBS = [
+    {
+        'id':1,
+        'title':'Data Analyst Intern',
+        'location': 'Champaign, IL',
+        'salary': '$16 / hour'
+    },
+    {
+        'id': 2,
+        'title':'Software Engineering Intern',
+        'location': 'Champaign, IL',
+        'salary': '$17 / hour'
+    },
+    {
+        'id': 3,
+        'title':'Front-End Engineer',
+        'location': 'Champaign, IL',
+        'salary': '$18 / hour'
+    },
+    {
+        'id': 4,
+        'title':'Software Engineer',
+        'location': 'Champaign, IL',
+    }
+]
+@app.route('/')
+
+#passing in JOBS as an argument gives us access to it in the html
+def start_of_everything():
+    return render_template('home.html', jobs = JOBS)
+
+#setting host to this makes the website accessible to any IP address. 
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',debug = True)
+
+
+
